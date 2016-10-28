45e984e985a87502e275bd0a48dabd8584bd435a
girish vadher <girish.vadher@code-desk.com>
2016-10-28 15:10:34 +0530

    alerts updated required filed

--- app/lib/Alert.php
+++ app/lib/Alert.php
@@ -12,6 +12,11 @@ class Alert extends \Eloquent{
     protected $table = "tblAlert";
     protected $primaryKey = "AlertID";
 
+    public static $qos_alert_type = array(''=>'Select','ACD'=>'ACD','ASR'=>'ASR');
+    public static $call_monitor_alert_type = array(''=>'Select','block_destination'=>'Blacklisted Destination','call_duration'=>'Longest Call','call_cost'=>'Expensive Calls','call_after_office'=>'Call After Business Hour');
+    public static $call_monitor_customer_alert_type = array(''=>'Select','call_duration'=>'Longest Call','call_cost'=>'Expensive Calls','call_after_office'=>'Call After Business Hour');
+    public static $call_blacklist_alert_type = array(''=>'Select','block_destination'=>'Blacklisted Destination');
+
     /** asr acd alerts */
     public static function asr_acd_alert($CompanyID, $ProcessID){
         $Alerts = Alert::where(array('CompanyID' => $CompanyID, 'AlertGroup' => 'qos', 'Status' => 1))->orderby('AlertType', 'asc')->get();
@@ -45,55 +50,48 @@ class Alert extends \Eloquent{
                 Log::info($query);
                 $ACD_ASR_alerts = DB::connection('neon_report')->select($query);
                 foreach ($ACD_ASR_alerts as $ACD_ASR_alert) {
+                    $AccountID = 0;
+                    $settings['AccountName'] = '';
+                    if(isset($ACD_ASR_alert->AccountID)) {
+                        $settings['AccountName'] = Account::getAccountName($ACD_ASR_alert->AccountID);
+                        $AccountID = $ACD_ASR_alert->AccountID;
+                    }
+
                     if ($Alert->AlertType == 'ACD' && !empty($ACD_ASR_alert->ACD) && ((!empty($Alert->LowValue) && $Alert->LowValue > $ACD_ASR_alert->ACD) || !empty($Alert->HighValue) && $Alert->HighValue < $ACD_ASR_alert->ACD)) {
-                        $settings['Subject'] = 'Qos Alert - ACD - '.$Alert->Name;
+                        $settings['Subject'] = 'Qos Alert - ACD ('.$Alert->Name.' )';
                         $settings['EmailType'] = AccountEmailLog::QosACDAlert;
                         $settings['EmailMessage'] = View::make('emails.qos_acd_alert',compact('ACD_ASR_alert','Alert','settings'))->render();
-                        if(isset($ACD_ASR_alert->AccountID)) {
-                            $settings['AccountName'] = Account::getAccountName($ACD_ASR_alert->AccountID);
-                            NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $ACD_ASR_alert->AccountID, $settings);
-                        }else{
-                            NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, 0, $settings);
-                        }
+                        NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $AccountID, $settings);
                     }
                     if ($Alert->AlertType == 'ASR' && !empty($ACD_ASR_alert->ASR) && ((!empty($Alert->LowValue) && $Alert->LowValue > $ACD_ASR_alert->ASR) || !empty($Alert->HighValue) && $Alert->HighValue < $ACD_ASR_alert->ASR)) {
-                        $settings['Subject'] = 'Qos Alert - ASR - '.$Alert->Name;
+                        $settings['Subject'] = 'Qos Alert - ASR ('.$Alert->Name.' )';
                         $settings['EmailType'] = AccountEmailLog::QosASRAlert;
                         $settings['EmailMessage'] = View::make('emails.qos_asr_alert',compact('ACD_ASR_alert','Alert','settings'))->render();
-                        if(isset($ACD_ASR_alert->AccountID)) {
-                            $settings['AccountName'] = Account::getAccountName($ACD_ASR_alert->AccountID);
-                            NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $ACD_ASR_alert->AccountID, $settings);
-                        }else{
-                            NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, 0, $settings);
-                        }
+                        NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $AccountID, $settings);
                     }
                 }
                 $query = "CALL prc_getVendorACD_ASR_Alert(" . $CompanyID.",'".$CompanyGatewayID."','".$VAccountID."','" .$CurrencyID."','" . $StartDate . "','" . $EndDate . "','".$AreaPrefix."','".$Trunk."','". $CountryID . "')";
                 Log::info($query);
                 $ACD_ASR_alerts = DB::connection('neon_report')->select($query);
                 foreach ($ACD_ASR_alerts as $ACD_ASR_alert) {
+                    $AccountID = 0;
+                    $settings['AccountName'] = '';
+                    if(isset($ACD_ASR_alert->AccountID)) {
+                        $settings['AccountName'] = Account::getAccountName($ACD_ASR_alert->AccountID);
+                        $AccountID = $ACD_ASR_alert->AccountID;
+                    }
                     if ($Alert->AlertType == 'ACD' && !empty($ACD_ASR_alert->ACD) && ((!empty($Alert->LowValue) && $Alert->LowValue > $ACD_ASR_alert->ACD) || !empty($Alert->HighValue) && $Alert->HighValue < $ACD_ASR_alert->ACD)) {
-                        $settings['Subject'] = 'Qos Vendor Alert - ACD - '.$Alert->Name;
+                        $settings['Subject'] = 'Qos Vendor Alert - ACD ('.$Alert->Name.' )';
                         $settings['EmailMessage'] = View::make('emails.qos_acd_alert',compact('ACD_ASR_alert','Alert','settings'))->render();
                         $settings['EmailType'] = AccountEmailLog::QosACDAlert;
-                        if(isset($ACD_ASR_alert->AccountID)) {
-                            $settings['AccountName'] = Account::getAccountName($ACD_ASR_alert->AccountID);
-                            NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $ACD_ASR_alert->AccountID, $settings);
-                        }else{
-                            NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, 0, $settings);
-                        }
+                        NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $AccountID, $settings);
 
                     }
                     if ($Alert->AlertType == 'ASR' && !empty($ACD_ASR_alert->ASR) && ((!empty($Alert->LowValue) && $Alert->LowValue > $ACD_ASR_alert->ASR) || !empty($Alert->HighValue) && $Alert->HighValue < $ACD_ASR_alert->ASR)) {
-                        $settings['Subject'] = 'Qos Vendor Alert - ASR - '.$Alert->Name;
+                        $settings['Subject'] = 'Qos Vendor Alert - ASR ('.$Alert->Name.' )';
                         $settings['EmailMessage'] = View::make('emails.qos_asr_alert',compact('ACD_ASR_alert','Alert','settings'))->render();
                         $settings['EmailType'] = AccountEmailLog::QosASRAlert;
-                        if(isset($ACD_ASR_alert->AccountID)) {
-                            $settings['AccountName'] = Account::getAccountName($ACD_ASR_alert->AccountID);
-                            NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $ACD_ASR_alert->AccountID, $settings);
-                        }else{
-                            NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, 0, $settings);
-                        }
+                        NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $AccountID, $settings);
                     }
                 }
                 NeonAlert::UpdateNextRunTime($Alert->AlertID, 'Settings', 'Alert', $settings['NextRunTime']);
@@ -109,6 +107,8 @@ class Alert extends \Eloquent{
         foreach ($Alerts as $Alert) {
             $settings = json_decode($Alert->Settings, true);
             $settings['ProcessID'] = $ProcessID;
+            $settings['Subject'] = 'Potential Fraud Alert ( '.Alert::$call_monitor_alert_type[$Alert->AlertType].' )';
+            Log::info($Alert->AlertType.' start');
             if ($Alert->AlertType == 'call_duration' && intval($settings['Duration']) > 0) {
                 self::CallDurationAlert($CompanyID,$Alert,$settings);
             } else if ($Alert->AlertType == 'call_cost' && intval($settings['Cost']) > 0) {
@@ -118,6 +118,7 @@ class Alert extends \Eloquent{
             } else if ($Alert->AlertType == 'block_destination' && !empty($settings['BlacklistDestination'])) {
                 self::CallBlackListAlert($CompanyID,$Alert,$settings);
             }
+            Log::info($Alert->AlertType . ' end ' );
         }
     }
 
@@ -133,19 +134,19 @@ class Alert extends \Eloquent{
         if ($call_duration_count > 0 || $vcall_duration_count>0) {
 
             $Account = Account::find($settings['AccountID']);
-
-            if(!empty($Account->BillingEmail)){
-                $call_duration = CDRPostProcess::where('AccountID', intval($settings['AccountID']))
-                    ->where('billed_duration', '>', intval($settings['Duration']))
-                    ->get();
-                $vcall_duration = VCDRPostProcess::where('AccountID', intval($settings['AccountID']))
-                    ->where('billed_duration', '>', intval($settings['Duration']))
-                    ->get();
-
+            $call_duration = CDRPostProcess::where('AccountID', intval($settings['AccountID']))
+                ->where('billed_duration', '>', intval($settings['Duration']))
+                ->get();
+            $vcall_duration = VCDRPostProcess::where('AccountID', intval($settings['AccountID']))
+                ->where('billed_duration', '>', intval($settings['Duration']))
+                ->get();
+            $settings['EmailMessage'] = View::make('emails.call_duration_alert',compact('call_duration','vcall_duration','settings'))->render();
+            $settings['EmailType'] = AccountEmailLog::CallDurationAlert;
+            if(!empty($settings['ReminderEmail'])){
+                NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID,$settings['AccountID'],$settings);
+            }
+            if($settings['EmailToAccount'] == 1 && !empty($Account->BillingEmail)){
                 $settings['ReminderEmail'] = $Account->BillingEmail;
-                $settings['EmailMessage'] = View::make('emails.call_duration_alert',compact('call_duration','vcall_duration','settings'))->render();
-                $settings['Subject'] = 'Potential Fraud Alert Call Duration';
-                $settings['EmailType'] = AccountEmailLog::CallDurationAlert;
                 NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID,$settings['AccountID'],$settings);
             }
         }
@@ -164,20 +165,23 @@ class Alert extends \Eloquent{
 
             $Account = Account::find($settings['AccountID']);
 
-            if(!empty($Account->BillingEmail)){
-                $call_cost = CDRPostProcess::where('AccountID', intval($settings['AccountID']))
-                    ->where('cost', '>', intval($settings['Cost']))
-                    ->get();
-                $vcall_cost = VCDRPostProcess::where('AccountID', intval($settings['AccountID']))
-                    ->where('buying_cost', '>', intval($settings['Cost']))
-                    ->get();
+            $call_cost = CDRPostProcess::where('AccountID', intval($settings['AccountID']))
+                ->where('cost', '>', intval($settings['Cost']))
+                ->get();
+            $vcall_cost = VCDRPostProcess::where('AccountID', intval($settings['AccountID']))
+                ->where('buying_cost', '>', intval($settings['Cost']))
+                ->get();
+            $settings['EmailMessage'] = View::make('emails.call_cost_alert',compact('call_cost','vcall_cost','settings'))->render();
+            $settings['EmailType'] = AccountEmailLog::CallCostAlert;
 
-                $settings['ReminderEmail'] = $Account->BillingEmail;
-                $settings['EmailMessage'] = View::make('emails.call_cost_alert',compact('call_cost','vcall_cost','settings'))->render();
-                $settings['Subject'] = 'Potential Fraud Alert Call Cost';
-                $settings['EmailType'] = AccountEmailLog::CallCostAlert;
+            if(!empty($settings['ReminderEmail'])){
                 NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID,$settings['AccountID'],$settings);
             }
+            if($settings['EmailToAccount'] == 1 && !empty($Account->BillingEmail)){
+                $settings['ReminderEmail'] = $Account->BillingEmail;
+                NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID,$settings['AccountID'],$settings);
+            }
+
         }
     }
 
@@ -198,24 +202,26 @@ class Alert extends \Eloquent{
 
         if ($call_office_count > 0 || $vcall_office_count > 0) {
             $Account = Account::find($settings['AccountID']);
-            if (!empty($Account->BillingEmail)) {
-                $call_office = CDRPostProcess::where('AccountID', intval($settings['AccountID']))
-                    ->Where(function ($query) use ($settings) {
-                        $query->whereRaw("time(connect_time) < '" . date("H:i:s", strtotime($settings['OpenTime'])) . "'")
-                            ->orwhereRaw("time(connect_time) > '" . date("H:i:s", strtotime($settings['CloseTime'])) . "'" );
-                    })
-                    ->get();
-                $vcall_office = VCDRPostProcess::where('AccountID', intval($settings['AccountID']))
-                    ->Where(function ($query) use ($settings) {
-                        $query->whereRaw("time(connect_time) < '" . date("H:i:s", strtotime($settings['OpenTime'])) . "'")
-                            ->orwhereRaw("time(connect_time) > '" . date("H:i:s", strtotime($settings['CloseTime'])) . "'" );
-                    })
-                    ->get();
+            $call_office = CDRPostProcess::where('AccountID', intval($settings['AccountID']))
+                ->Where(function ($query) use ($settings) {
+                    $query->whereRaw("time(connect_time) < '" . date("H:i:s", strtotime($settings['OpenTime'])) . "'")
+                        ->orwhereRaw("time(connect_time) > '" . date("H:i:s", strtotime($settings['CloseTime'])) . "'" );
+                })
+                ->get();
+            $vcall_office = VCDRPostProcess::where('AccountID', intval($settings['AccountID']))
+                ->Where(function ($query) use ($settings) {
+                    $query->whereRaw("time(connect_time) < '" . date("H:i:s", strtotime($settings['OpenTime'])) . "'")
+                        ->orwhereRaw("time(connect_time) > '" . date("H:i:s", strtotime($settings['CloseTime'])) . "'" );
+                })
+                ->get();
+            $settings['EmailMessage'] = View::make('emails.call_office_alert', compact('call_office', 'vcall_office','settings'))->render();
+            $settings['EmailType'] = AccountEmailLog::CallCostAlert;
+            if(!empty($settings['ReminderEmail'])){
+                NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID,$settings['AccountID'],$settings);
+            }
+            if($settings['EmailToAccount'] == 1 && !empty($Account->BillingEmail)){
                 $settings['ReminderEmail'] = $Account->BillingEmail;
-                $settings['EmailMessage'] = View::make('emails.call_office_alert', compact('call_office', 'vcall_office','settings'))->render();
-                $settings['Subject'] = 'Potential Fraud Alert Call Office';
-                $settings['EmailType'] = AccountEmailLog::CallCostAlert;
-                NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, $settings['AccountID'], $settings);
+                NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID,$settings['AccountID'],$settings);
             }
         }
     }
@@ -233,7 +239,6 @@ class Alert extends \Eloquent{
 
 
                 $settings['EmailMessage'] = View::make('emails.call_blacklist_alert', compact('call_blacklist', 'vcall_blacklist','settings'))->render();
-                $settings['Subject'] = 'Potential Fraud Alert Call Black List';
                 $settings['EmailType'] = AccountEmailLog::CallCostAlert;
                 NeonAlert::SendReminderToEmail($CompanyID, $Alert->AlertID, 0, $settings);
             }
