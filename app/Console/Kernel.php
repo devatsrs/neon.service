92ac91684c75c64bf2e00b0cf33febdd9573e43d
deven <shriramsoft@gmail.com>
2016-12-15 17:49:22 +0530

    Generate Rate Update file update - separate vendor customer cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -71,7 +71,8 @@ class Kernel extends ConsoleKernel {
 		'App\Console\Commands\ServerCleanUp',
 		'App\Console\Commands\AccountReadEmails',
 		'App\Console\Commands\QuickBookInvoicePost',
-		'App\Console\Commands\GenerateRateUpdateFile',
+		'App\Console\Commands\GenerateCustomerRateUpdateFile',
+		'App\Console\Commands\GenerateVendorRateUpdateFile',
 		'App\Console\Commands\PBXAccountBlock',
 	];
 
