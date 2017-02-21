9f42172185ab349af222702da8e4062fcfa9ed9e
deven <shriramsoft@gmail.com>
2017-02-21 16:37:02 +0530

    deployment script

--- bin/deployment/generate_fresh_db_setup_script.sh
+++ bin/deployment/generate_fresh_db_setup_script.sh
@@ -43,8 +43,8 @@ mysqldump --compact --no-create-info -w 'CodeDeckId = 1' ${WHOLESALE_RM_DB} tblR
 
 echo "Cleanup sql file for new DBs."
 
-sed -i 's/utf8mb4_general_ci/utf8_unicode_ci/g' POST_INSTALLATION_SQL_SCRIPT
-sed -i 's/utf8mb4/utf8/g' POST_INSTALLATION_SQL_SCRIPT
-sed -i 's/ AUTO_INCREMENT=[0-9]*//g' POST_INSTALLATION_SQL_SCRIPT
+sed -i 's/utf8mb4_general_ci/utf8_unicode_ci/g' ${POST_INSTALLATION_SQL_SCRIPT}
+sed -i 's/utf8mb4/utf8/g' ${POST_INSTALLATION_SQL_SCRIPT}
+sed -i 's/ AUTO_INCREMENT=[0-9]*//g' ${POST_INSTALLATION_SQL_SCRIPT}
 
 exit 0;
