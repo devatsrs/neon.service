3eacedcad0f5553001a95d17af1a4e54f5d6077a
girish vadher <girish.vadher@code-desk.com>
2017-03-31 17:02:11 +0530

    NEON-824

--- app/lib/EmailsTemplates.php
+++ app/lib/EmailsTemplates.php
@@ -36,6 +36,7 @@ class EmailsTemplates{
 				"{{CompanyCity}}",
 				"{{CompanyPostCode}}",
 				"{{CompanyCountry}}",
+				"{{Logo}}",
 				);
 	
 	 public function __construct($data = array()){
@@ -160,6 +161,7 @@ class EmailsTemplates{
 			$array['CompanyCity']					=   $CompanyData->City;
 			$array['CompanyPostCode']				=   $CompanyData->PostCode;
 			$array['CompanyCountry']				=   $CompanyData->Country;			
+			$array['Logo'] 							= \App\Lib\CompanyConfiguration::get($CompanyID,'WEB_URL').'/assets/images/logo@2x.png'; 
 			return $array;
 	}
 	
@@ -190,7 +192,9 @@ class EmailsTemplates{
 					{
 						$array['Signature']= $UserData->EmailFooter;	
 					}
-	        	}
+	        	}else{
+					$array['Signature']= '';	
+				}
 			return $array;
 	}
 	
