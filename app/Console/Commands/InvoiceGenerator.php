bf3c4034f92dd0904e41f7a2abacf75cbe59fd2c
girish vadher <girish.vadher@code-desk.com>
2017-03-31 18:54:14 +0530

    Roll back service from staging.

--- app/Console/Commands/InvoiceGenerator.php
+++ app/Console/Commands/InvoiceGenerator.php
@@ -1,6 +1,9 @@
 <?php namespace App\Console\Commands;
 
 use App\Lib\Account;
+use App\Lib\AccountBilling;
+use App\Lib\AccountNextBilling;
+use App\Lib\CompanySetting;
 use App\Lib\CronHelper;
 use App\Lib\CronJob;
 use App\Lib\CronJobLog;
@@ -10,15 +13,18 @@ use App\Lib\JobStatus;
 use App\Lib\JobType;
 use App\Lib\Product;
 use App\Lib\RecurringInvoice;
+use App\Lib\RecurringInvoiceLog;
 use App\Lib\TaxRate;
 use App\Lib\User;
-use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 use Webpatser\Uuid\Uuid;
+use App\Lib\Helper;
+use App\Lib\Company;
+use \Exception;
 
 class InvoiceGenerator extends Command {
 
@@ -67,8 +73,11 @@ class InvoiceGenerator extends Command {
         $cronsetting = json_decode($CronJob->Settings,true);
         $today = date("Y-m-d");
         $date = date('Y-m-d H:i:s');
-        CronJob::activateCronJob($CronJob);
-        CronJob::createLog($CronJobID);
+
+        $dataactive['Active'] = 1;
+        $dataactive['PID'] = $getmypid;
+        $dataactive['LastRunTime'] = date('Y-m-d H:i:00');
+        $CronJob->update($dataactive);
 
         $joblogdata = array();
         $joblogdata['CronJobID'] = $CronJobID;
@@ -97,7 +106,8 @@ class InvoiceGenerator extends Command {
         $InvoiceGenerationEmail = isset($cronsetting['SuccessEmail']) ? $cronsetting['SuccessEmail'] :'';
         $InvoiceGenerationEmail = explode(",",$InvoiceGenerationEmail);
 
-
+        $errors = array();
+        $message = array();
 
         $AccountIDs = array_pluck($Accounts, 'AccountID');
 
@@ -133,16 +143,149 @@ class InvoiceGenerator extends Command {
         }
         /** ************************************************************/
 
+
+        Log::error(' ========================== Invoice Send Loop Start =============================');
+        $skip_accounts = array();
         try {
+            CronJob::createLog($CronJobID);
+            do{
+            $Accounts = Account::join('tblAccountBilling','tblAccountBilling.AccountID','=','tblAccount.AccountID')
+                ->select(["tblAccountBilling.AccountID","tblAccountBilling.NextInvoiceDate","AccountName"])
+                ->whereNotIn('tblAccount.AccountID',$skip_accounts)
+                ->where(["CompanyID" =>$CompanyID, "Status" => 1,"AccountType" => 1,"Billing"=>1 ])
+                ->where('tblAccountBilling.NextInvoiceDate','<>','')
+                ->where('tblAccountBilling.NextInvoiceDate','<>','0000-00-00')
+                ->where('tblAccountBilling.NextInvoiceDate','<=',$today)
+                ->whereNotNull('tblAccountBilling.BillingCycleType')
+                ->orderby('tblAccount.AccountID')->get();
+            foreach ($Accounts as $Account) {
 
-            /** regular invoice start */
-            $response = Invoice::GenerateInvoice($CompanyID,$InvoiceGenerationEmail,$ProcessID, $JobID);
-            $errors = isset($response['errors'])?$response['errors']:array();
-            $message = isset($response['message'])?$response['message']:array();
+                $AccountName = $Account['AccountName'];
+                $AccountID = $Account['AccountID'];
+                try {
 
 
 
+                    $NextInvoiceDate = Invoice::getNextInvoiceDate($CompanyID, $AccountID);
+                    Log::info('AccountID =' . $AccountID . ' NextInvoiceDate = ' . $NextInvoiceDate);
 
+                    if (!empty($NextInvoiceDate)) {
+
+                        $EndDate = date("Y-m-d", strtotime("-1 Day", strtotime($NextInvoiceDate)));
+                        /**
+                         * 1. If Account is not in tblAccountGateway Generate Invoice
+                         * 2. If Account is present check **cdr download log** end date > $EndDate (1-11-2015 > 31-10-2015)
+                         * 3. If no calling in CDR then also log is generated so checking in log only. not in tblUsageDetails so if customer is
+                         * not sending any traffic then also 0 value invoice is generated.
+                         * */
+                        $isCDRLoaded = DB::connection('sqlsrv2')->select("CALL prc_checkCDRIsLoadedOrNot(" . (int)$AccountID . ",$CompanyID,'$EndDate')");
+
+                        Log::info('isCDRLoaded ' . print_r($isCDRLoaded, true));
+
+                        if (strtotime($NextInvoiceDate) <= strtotime(date("Y-m-d"))) {
+
+                            if (isset($isCDRLoaded[0]->isLoaded) && $isCDRLoaded[0]->isLoaded == 1 ) {
+
+                                Log::info(' ========================== Invoice Send Start =============================');
+
+
+                                if (strtotime($NextInvoiceDate) <= strtotime(date("Y-m-d"))) {
+                                    DB::beginTransaction();
+                                    DB::connection('sqlsrv2')->beginTransaction();
+                                    Log::info('AccountID =' . $AccountID . ' NextInvoiceDate = ' . $NextInvoiceDate);
+
+                                    $LastInvoiceDate = Invoice::getLastInvoiceDate($CompanyID, $AccountID);
+
+                                    Log::info('getLastInvoiceDate =' . $LastInvoiceDate);
+
+                                    /*                    $StartDate = $LastInvoiceDate;
+                                                        $EndDate = date("Y-m-d 23:59:59", strtotime("-1 Day", strtotime($NextInvoiceDate)));*/
+
+                                    Log::info('Invoice::sendInvoice(' . $CompanyID . ',' . $AccountID . ',' . $LastInvoiceDate . ',' . $NextInvoiceDate . ',' . implode(",", $InvoiceGenerationEmail) . ')');
+
+                                    $response = Invoice::sendInvoice($CompanyID, $AccountID, $LastInvoiceDate, $NextInvoiceDate, $InvoiceGenerationEmail,$ProcessID,$JobID);
+
+                                    Log::info('Invoice::sendInvoice done');
+
+                                    if (isset($response["status"]) && $response["status"] == 'success') {
+
+                                        Log::info('Invoice created - ' . print_r($response, true));
+                                        $message[] = $response["message"];
+                                        Log::info('Invoice Commited  AccountID = ' . $AccountID);
+                                        DB::connection('sqlsrv2')->commit();
+                                        Log::info('=========== Updating  InvoiceDate =========== ');
+                                        $AccountBilling = AccountBilling::getBilling($AccountID);
+                                        $oldNextInvoiceDate = $NextInvoiceDate;
+                                        $NewNextInvoiceDate = next_billing_date($AccountBilling->BillingCycleType,$AccountBilling->BillingCycleValue,strtotime($oldNextInvoiceDate));
+                                        AccountBilling::where(['AccountID'=>$AccountID])->update(["LastInvoiceDate"=>$oldNextInvoiceDate,"NextInvoiceDate"=>$NewNextInvoiceDate]);
+                                        $AccountNextBilling = AccountNextBilling::getBilling($AccountID);
+                                        if(!empty($AccountNextBilling)){
+                                            AccountBilling::where(['AccountID'=>$AccountID])->update(["BillingCycleType"=>$AccountNextBilling->BillingCycleType,"BillingCycleValue"=>$AccountNextBilling->BillingCycleValue,'LastInvoiceDate'=>$AccountNextBilling->LastInvoiceDate,'NextInvoiceDate'=>$AccountNextBilling->NextInvoiceDate]);
+                                            AccountNextBilling::where(['AccountID'=>$AccountID])->delete();
+                                        }
+
+
+                                        Log::info('=========== Updated  InvoiceDate =========== ') ;
+                                        DB::commit();
+
+                                    } else {
+                                        $errors[] = $response["message"];
+                                        DB::rollback();
+                                        DB::connection('sqlsrv2')->rollback();
+                                        $skip_accounts[] = $AccountID;
+                                        Log::info('Invoice rollback  AccountID = ' . $AccountID);
+                                        Log::info(' ========================== Error  =============================');
+                                        Log::info('Invoice with Error - ' . print_r($response, true));
+
+                                    }
+                                }
+                            }else {
+                                $errors[] = $AccountName . " " . Invoice::$InvoiceGenrationErrorReasons["NoCDR"];
+                                $skip_accounts[] = $AccountID;
+                                continue;
+                            }
+                        }else{
+                            $skip_accounts[] = $AccountID;
+                        }
+
+                    }else{
+                        $skip_accounts[] = $AccountID;
+                    }
+
+                    Log::info(' ========================== Invoice Send End =============================');
+
+
+                } catch (\Exception $e) {
+
+                    try {
+                        $skip_accounts[] = $AccountID;
+                        Log::error('Invoice Rollback AccountID = ' . $AccountID);
+                        DB::rollback();
+                        DB::connection('sqlsrv2')->rollback();
+                        Log::error($e);
+
+                        $errors[] = $AccountName . " " . $e->getMessage();
+
+
+                    }catch (Exception $err) {
+                        Log::error($err);
+                        $errors[] = $AccountName . " " . $e->getMessage() . ' ## ' . $err->getMessage();
+                    }
+
+                }
+
+            } // Loop over
+                //Log::info($skip_accounts);
+        }while(Account::join('tblAccountBilling','tblAccountBilling.AccountID','=','tblAccount.AccountID')
+                ->select(["tblAccount.AccountID","AccountName"])
+                ->where(["CompanyID" =>$CompanyID, "Status" => 1,"AccountType" => 1,"Billing"=>1 ])
+                ->where('tblAccountBilling.NextInvoiceDate','<>','')
+                ->where('tblAccountBilling.NextInvoiceDate','<>','0000-00-00')
+                ->where('tblAccountBilling.NextInvoiceDate','<=',$today)
+                ->whereNotIn('tblAccount.AccountID',$skip_accounts)
+                ->whereNotNull('tblAccountBilling.BillingCycleType')->count());
+
+            Log::info(' ========================== Invoice Send Loop End =============================');
 
             /** recurring invoice generation start*/
             $recuringerrors = RecurringInvoice::GenerateRecurringInvoice($CompanyID,$ProcessID,$UserID,$date,$JobID,$InvoiceGenerationEmail,$data);
@@ -208,7 +351,9 @@ class InvoiceGenerator extends Command {
                 Log::error("**Email Sent message " . $result['message']);
             }
         }
-        CronJob::deactivateCronJob($CronJob);
+        $dataactive['Active'] = 0;
+        $dataactive['PID'] = '';
+        $CronJob->update($dataactive);
         if(!empty($cronsetting['SuccessEmail'])){
             $result = CronJob::CronJobSuccessEmailSend($CronJobID);
             Log::error("**Email Sent Status ".$result['status']);
