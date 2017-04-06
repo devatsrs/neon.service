9fd5907db5402c5776ddfb495b69e79e80a2d532
deven <shriramsoft@gmail.com>
2017-04-06 12:10:03 +0530

    autodeployment for update

--- bin/deployment/composer_n_permission.sh
+++ bin/deployment/composer_n_permission.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
-echo -e " Running composer update on " ${WEB_LOCATION} " --no-plugins --no-scripts";
+if [ "$COMPOSER_UPDATE" = "1" ]; then
 
-composer update -d ${WEB_LOCATION} --no-plugins --no-scripts
+    echo -e " Running composer update on " ${WEB_LOCATION} " --no-plugins --no-scripts";
 
-echo -e " Running composer update on " ${SERVICE_LOCATION}  " --no-plugins --no-scripts";
+    composer update -d ${WEB_LOCATION} --no-plugins --no-scripts
 
-composer update -d  ${SERVICE_LOCATION} --no-plugins --no-scripts
+    echo -e " Running composer update on " ${SERVICE_LOCATION}  " --no-plugins --no-scripts";
 
+    composer update -d  ${SERVICE_LOCATION} --no-plugins --no-scripts
 
-echo -e " Running composer update on " ${API_LOCATION}  " --no-plugins --no-scripts";
+    echo -e " Running composer update on " ${API_LOCATION}  " --no-plugins --no-scripts";
 
-composer update -d  ${API_LOCATION} --no-plugins --no-scripts
+    composer update -d  ${API_LOCATION} --no-plugins --no-scripts
+
+else
+
+    echo "composer update skipped..."
+
+fi
+
 
 echo "replace composer folders for web and service."
 
@@ -21,24 +29,24 @@ cp -rf ${SCRIPT_BASEDIR}/service_composer/* ${SERVICE_LOCATION}/vendor/
 
 git config core.fileMode false
 #for vos ssh file collect.
-git checkout -- ${SERVICE_LOCATION}/vendor/laravelcollective/remote/src
+cd ${SERVICE_LOCATION} && git checkout -- 'vendor/laravelcollective/remote/src'
 
 
 
 
 echo "Creating folders and Adding Permissions to folders"
 
-chmod -R 777 ${WEB_LOCATION}
+chmod -R 0777 ${WEB_LOCATION}
 
-chmod -R 777 ${SERVICE_LOCATION}
-
-
+chmod -R 0777 ${SERVICE_LOCATION}
 
 
-chmod -R 777 ${TEMP_PATH}
-chmod -R 777 ${WEB_LOCATION}
-chmod -R 777 ${SERVICE_LOCATION}
+
+
+#chmod -R 0777 ${TEMP_PATH}
+chmod -R 0777 ${WEB_LOCATION}
+chmod -R 0777 ${SERVICE_LOCATION}
 
 chown -R apache:apache ${WEB_LOCATION}
 chown -R apache:apache ${SERVICE_LOCATION}
-chown -R apache:apache ${TEMP_PATH}
+#chown -R apache:apache ${TEMP_PATH}
--- bin/deployment/update_env.sh
+++ bin/deployment/update_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "loading configuration..."
-source $(dirname "$0")/config.sh
+source $(dirname "$0")/update_config.sh
 
 #echo "stoping crond..."
 #service crond stop
