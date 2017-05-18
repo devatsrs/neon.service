b289e29c78b8b86082420bdbf04cbb882b3b313e
deven <shriramsoft@gmail.com>
2017-05-18 12:50:28 +0530

    NEON-914 Dispatcher Section in ticketing

--- app/lib/TicketImportRule.php
+++ app/lib/TicketImportRule.php
@@ -1,26 +1,54 @@
 <?php
 namespace App\Lib;
 
-use Illuminate\Database\Eloquent\Model;
-
-class TicketImportRule extends Model {
+class TicketImportRule extends \Eloquent  {
 
     protected $table 		= 	"tblTicketImportRule";
     protected $primaryKey 	= 	"TicketImportRuleID";
     protected $guarded 		=	 array("TicketImportRuleID");
 
+    const MATCH_ANY         = 1;
+    const MATCH_ALL         = 2;
 
-    public static function check($CompanyID,$TicketID){
+    public static function check($CompanyID,$TicketData){
 
-        $Ticket = TicketsTable::where(["TicketID"=>$TicketID])->get(["CompanyID"]);
-        $Ticket = json_decode(json_encode($Ticket),true);
 
         $TicketImportRules =  TicketImportRule::where(["CompanyID"=>$CompanyID,"Status"=>1])->get();
 
-        foreach($TicketImportRules as $TicketImportRule ){
+        if(count($TicketImportRules) > 0) {
+            foreach ($TicketImportRules as $TicketImportRule) {
+                $TicketImportRuleID = $TicketImportRule["TicketImportRuleID"];
+                $Match = $TicketImportRule["Match"];
 
-            print_r($TicketImportRule);
+                $TicketImportRuleConditions = TicketImportRuleCondition::where(["TicketImportRuleID" => $TicketImportRuleID])->orderby("Order")->get();
+                if(count($TicketImportRuleConditions) > 0) {
+                    foreach ($TicketImportRuleConditions as $TicketImportRuleCondition) {
 
+                        $TicketImportRuleConditionTypeID = $TicketImportRuleCondition["TicketImportRuleConditionTypeID"];
+                        $Operand = $TicketImportRuleCondition["Operand"];
+                        $Value = $TicketImportRuleCondition["Value"];
+
+                        if($TicketImportRuleConditionTypeID > 0 && with(new TicketImportRuleConditionType())->isEmailFrom($TicketImportRuleConditionTypeID)) {
+
+                            $fromEmail = $TicketData["Requester"];
+                            if( TicketImportRuleCondition::if($fromEmail)->is($Operand)->value($Value) ) {
+
+                                $result = TicketImportRuleAction::doActions($TicketData);
+
+                                if($result && $Match == self::MATCH_ANY) {
+                                    //$result["action"] =
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+                }
+
+
+            }
         }
-    }
+     }
 }
---
+++ app/lib/TicketImportRuleConditionType.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Lib;
+
+use Illuminate\Support\Facades\Cache;
+
+class TicketImportRuleConditionType extends \Eloquent  {
+
+    protected $table 		= 	"tblTicketImportRuleConditionType";
+    protected $primaryKey 	= 	"TicketImportRuleConditionTypeID";
+    protected $guarded 		=	 array("TicketImportRuleConditionTypeID");
+
+    const EMAIL_FROM = 'from_email';
+    const EMAIL_TO = 'to_email';
+    const SUBJECT = 'subject';
+    const DESCRIPTION = 'description';
+    const DESC_OR_SUB = 'subject_or_description';
+    const PRIORITY = 'priority';
+    const STATUS = 'status';
+    const AGENT = 'agent';
+    const GROUP = 'group';
+
+    protected $enable_cache = true;
+    protected $cache_name = "TicketImportRuleConditionType";
+
+    // load all types in cache
+    function __construct(){
+
+        if ($this->enable_cache && Cache::has($this->cache_name)) {
+
+            $cache = Cache::get($this->cache_name);
+
+        } else {
+            $cache = array();
+            $cache[$this->cache_name] = TicketImportRuleConditionType::lists('Condition','TicketImportRuleConditionTypeID');
+            Cache::forever($this->cache_name, $cache);
+
+        }
+        return $cache[$this->cache_name];
+    }
+
+    // get type value by id
+    function get($key){
+
+        $cache = Cache::get($this->cache_name);
+        if(!empty($cache) && isset($cache[$key])){
+                return $cache[$key];
+        }
+        return "";
+
+    }
+
+    function isEmailFrom($TicketImportRuleConditionTypeID){
+
+        if($this->get($TicketImportRuleConditionTypeID) == self::EMAIL_FROM){
+            return true;
+        }
+        return false;
+    }
+ }
