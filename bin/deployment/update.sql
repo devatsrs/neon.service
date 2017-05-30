a307543748a404af82b9ae51c01bf8d047bfba7c
deven <shriramsoft@gmail.com>
2017-05-30 15:12:40 +0530

    4.11

--- bin/deployment/update.sql
+++ bin/deployment/update.sql
@@ -515,7 +515,7 @@ SELECT AccountID,1,CompanyId,1 FROM tblAccount WHERE AccountType = 1 AND Status 
 UPDATE RMBilling3.tblAccountSubscription SET ServiceID =1 WHERE ServiceID = 0;
 UPDATE RMBilling3.tblAccountOneOffCharge SET ServiceID =1 WHERE ServiceID = 0;
 UPDATE tblCLIRateTable SET ServiceID =1 WHERE CompanyID =1 AND ServiceID = 0;
-UPDATE tblAccountDiscountPlan SET ServiceID =1 WHERE CompanyID =1 AND ServiceID = 0;
+UPDATE tblAccountDiscountPlan SET ServiceID =1 WHERE ServiceID = 0;
 
 END IF;
 
@@ -12700,6 +12700,52 @@ BEGIN
    DEALLOCATE PREPARE stmt2;
 	
    SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ; 
+END|
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `prc_DeleteDuplicateUniqueID2`;
+
+DELIMITER |
+
+CREATE PROCEDURE `prc_DeleteDuplicateUniqueID2`(
+	IN `p_CompanyID` INT,
+	IN `p_CompanyGatewayID` INT,
+	IN `p_ProcessID` VARCHAR(200),
+	IN `p_tbltempusagedetail_name` VARCHAR(200)
+)
+BEGIN
+
+	SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+	SET @stm1 = CONCAT('
+		DELETE tud FROM `' , p_tbltempusagedetail_name , '` tud
+		INNER JOIN tblVendorCDR ud ON tud.ID =ud.ID
+		INNER JOIN  tblVendorCDRHeader uh on uh.VendorCDRHeaderID = ud.VendorCDRHeaderID
+			AND tud.CompanyID = uh.CompanyID
+			AND tud.CompanyGatewayID = uh.CompanyGatewayID
+		WHERE tud.CompanyID = "' , p_CompanyID , '"
+		AND tud.CompanyGatewayID = "' , p_CompanyGatewayID , '"
+		AND tud.ProcessID = "' , p_processId , '";
+	');
+	PREPARE stmt1 FROM @stm1;
+	EXECUTE stmt1;
+	DEALLOCATE PREPARE stmt1;
+
+	SET @stm2 = CONCAT('
+		DELETE tud FROM `' , p_tbltempusagedetail_name , '` tud
+		INNER JOIN tblVendorCDRFailed ud ON tud.ID =ud.ID
+		INNER JOIN  tblVendorCDRHeader uh on uh.VendorCDRHeaderID = ud.VendorCDRHeaderID
+			AND tud.CompanyID = uh.CompanyID
+			AND tud.CompanyGatewayID = uh.CompanyGatewayID
+		WHERE tud.CompanyID = "' , p_CompanyID , '"
+		AND tud.CompanyGatewayID = "' , p_CompanyGatewayID , '"
+		AND tud.ProcessID = "' , p_processId , '";
+	');
+	PREPARE stmt2 FROM @stm2;
+	EXECUTE stmt2;
+	DEALLOCATE PREPARE stmt2;
+
+	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 END|
 DELIMITER ;
 
