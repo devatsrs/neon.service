d4a93026ae08e3d1f55908a66691dcd99b9ae9f0
Deven Sitapara <shriramsoft@gmail.com>
2017-06-08 18:49:56 +0530

    skip auto generated check when parent found
    NeonNEON-987
    Issue with Auto generated email

--- app/lib/Imap.php
+++ app/lib/Imap.php
@@ -58,17 +58,7 @@ protected $server;
 				$message_id   	= 		isset($overview[0]->message_id)?$overview[0]->message_id:'';
 				$references   	=  		isset($overview[0]->references)?$overview[0]->references:'';
 				$in_reply_to  	= 		isset($overview[0]->in_reply_to)?$overview[0]->in_reply_to:$message_id;
-
-				//-- check in reply to with previous email
-				// if exists then don't check for auto reply
-				$in_reply_tos   = explode(PHP_EOL,$in_reply_to);
-				foreach($in_reply_tos as $in_reply_to_id){
-
-					$msg_parent   	=		AccountEmailLog::where("MessageID",$in_reply_to_id)->first();
-					if(!empty($msg_parent) && isset($msg_parent->AccountEmailLogID)){
-						break;
-					}
-				}
+				$msg_parent   	=		AccountEmailLog::where("MessageID",$in_reply_to)->first();
 
 			
 				if(!empty($msg_parent)){ // if email is reply of an email
@@ -503,9 +493,18 @@ protected $server;
 				$header 					= 		  imap_fetchheader($inbox, $email_number);
 				$message_id   				= 		  isset($overview[0]->message_id)?$overview[0]->message_id:'';
 				$references   				=  		  isset($overview[0]->references)?$overview[0]->references:'';
-				$in_reply_to  				= 		  isset($overview[0]->in_reply_to)?$overview[0]->in_reply_to:$message_id;		
-					
-				$msg_parent   				=		  AccountEmailLog::where(["MessageID"=>$in_reply_to])->first();				
+				$in_reply_to  				= 		  isset($overview[0]->in_reply_to)?$overview[0]->in_reply_to:$message_id;
+
+				//-- check in reply to with previous email
+				// if exists then don't check for auto reply
+				$in_reply_tos   = explode(PHP_EOL,$in_reply_to);
+				foreach($in_reply_tos as $in_reply_to_id){
+
+					$msg_parent   	=		AccountEmailLog::where("MessageID",$in_reply_to_id)->first();
+					if(!empty($msg_parent) && isset($msg_parent->AccountEmailLogID)){
+						break;
+					}
+				}
 				
 				$headerdata					=		  imap_headerinfo($inbox, $email_number);		
 				//$msg_parentconversation   	=		  TicketsConversation::where("MessageID",$in_reply_to)->first();
