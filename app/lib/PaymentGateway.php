c94ac3b76331cc89ca43ee8e3d5b4eed2deac81e
Deven Sitapara <shriramsoft@gmail.com>
2017-06-14 15:29:54 +0530

    spelling mistake fix

--- app/lib/PaymentGateway.php
+++ app/lib/PaymentGateway.php
@@ -60,7 +60,7 @@ class PaymentGateway extends \Eloquent {
 		$AuthorizeData 					= 	\App\Lib\SiteIntegration::CheckIntegrationConfiguration(true,\App\Lib\SiteIntegration::$AuthorizeSlug,$CompanyID);
 		$AUTHORIZENET_API_LOGIN_ID  	= 	isset($AuthorizeData->AuthorizeLoginID)?$AuthorizeData->AuthorizeLoginID:'';		
 		$AUTHORIZENET_TRANSACTION_KEY  	= 	isset($AuthorizeData->AuthorizeTransactionKey)?$AuthorizeData->AuthorizeTransactionKey:'';
-		$isSandbox						=	isset($AuthorizeDbData->AuthorizeTestAccount)?$AuthorizeDbData->AuthorizeTestAccount:'';
+		$isSandbox						=	isset($AuthorizeData->AuthorizeTestAccount)?$AuthorizeData->AuthorizeTestAccount:'';
 		
         define("AUTHORIZENET_API_LOGIN_ID", $AUTHORIZENET_API_LOGIN_ID);
         define("AUTHORIZENET_TRANSACTION_KEY", $AUTHORIZENET_TRANSACTION_KEY);
