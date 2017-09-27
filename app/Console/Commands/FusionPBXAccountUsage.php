b775db2c97c22a4064e7e1d3bd1cf9216458d277
girish vadher <girish.vadher@code-desk.com>
2017-09-27 15:21:17 +0530

    #1081 Fusion PBX Integration

--- app/Console/Commands/FusionPBXAccountUsage.php
+++ app/Console/Commands/FusionPBXAccountUsage.php
@@ -290,7 +290,7 @@ class FusionPBXAccountUsage extends Command {
             $endtime = date('Y-m-d H:i:s', strtotime('-'.$usageinterval.' minute'));  //date('Y-m-d H:i:s');
         }
         if (empty($endtime)) {
-            $endtime = date('Y-m-1 00:00:00');
+            $endtime = date('Y-m-01 00:00:00');
         }
         return $endtime;
     }
