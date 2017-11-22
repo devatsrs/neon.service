d53ebdbbdc783ca77173829c1a6b49d28e6d165c
girish vadher <girish.vadher@code-desk.com>
2017-11-22 17:24:05 +0530

    #1194  margin report

--- bin/deployment/update.sql
+++ bin/deployment/update.sql
@@ -13290,6 +13290,8 @@ BEGIN
 
 	SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;
 	
+	SET SESSION innodb_lock_wait_timeout = 600;
+	
 	IF p_Type = 'Customer'
 	THEN
 		SET @stmt = CONCAT('
@@ -13306,24 +13308,6 @@ BEGIN
 		EXECUTE stmt;
 		DEALLOCATE PREPARE stmt;
 		
-		/*SET @stmt = CONCAT('
-		UPDATE tblTempCallDetail_1_',p_UniqueID,' uh
-		INNER JOIN RMBilling3.tblGatewayAccount ga
-			ON  uh.GatewayAccountPKID = ga.GatewayAccountPKID
-		SET uh.AccountID = ga.AccountID
-		WHERE uh.AccountID IS NULL
-		AND ga.AccountID is not null;
-		');
-
-		PREPARE stmt FROM @stmt;
-		EXECUTE stmt;
-		DEALLOCATE PREPARE stmt;*/
-
-	END IF;
-
-	IF p_Type = 'Vendor'
-	THEN
-
 		SET @stmt = CONCAT('
 		UPDATE tmp_tblVendorUsageDetailsReport_',p_UniqueID,' uh
 		INNER JOIN RMBilling3.tblGatewayAccount ga
@@ -13338,20 +13322,8 @@ BEGIN
 		EXECUTE stmt;
 		DEALLOCATE PREPARE stmt;
 		
-		/*SET @stmt = CONCAT('
-		UPDATE tblTempCallDetail_2_',p_UniqueID,' uh
-		INNER JOIN RMBilling3.tblGatewayAccount ga
-			ON  uh.GatewayVAccountPKID = ga.GatewayAccountPKID
-		SET uh.VAccountID = ga.AccountID
-		WHERE uh.VAccountID IS NULL
-		AND ga.AccountID is not null;
-		');
-
-		PREPARE stmt FROM @stmt;
-		EXECUTE stmt;
-		DEALLOCATE PREPARE stmt;*/
-
 	END IF;
+	
 
 	SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 
