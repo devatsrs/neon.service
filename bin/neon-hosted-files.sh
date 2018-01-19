9f2895c04605317719b2e727ac916335e24b45fe
Deven Sitapara <shriramsoft@gmail.com>
2018-01-19 13:11:41 +0530

    file backup update

--- bin/neon-hosted-files.sh
+++ bin/neon-hosted-files.sh
@@ -3,17 +3,19 @@
 #Hosted server files
 
 /usr/bin/s3cmd put --recursive /home/hostedfolders/codedesk/tmp/1 s3://neon.hosted.files/codedesk/
-/usr/bin/s3cmd put --recursive /home/hostedfolders/covercommunications/tmp/1 s3://neon.hosted.files/covercommunications/
-#/usr/bin/s3cmd put --recursive /home/hostedfolders/shaztech/tmp/1 s3://neon.hosted.files/shaztech/
 /usr/bin/s3cmd put --recursive /home/hostedfolders/twistvoip/tmp/1 s3://neon.hosted.files/twistvoip/
 /usr/bin/s3cmd put --recursive /home/hostedfolders/voicecourier/tmp/1 s3://neon.hosted.files/voicecourier/
 /usr/bin/s3cmd put --recursive /home/hostedfolders/pbxline/tmp/1 s3://neon.hosted.files/pbxline/
-/usr/bin/s3cmd put --recursive /home/hostedfolders/usmantel/tmp/1 s3://neon.hosted.files/usmantel/
-/usr/bin/s3cmd put --recursive /home/hostedfolders/familytalk/tmp/1 s3://neon.hosted.files/familytalk/
+/usr/bin/s3cmd put --recursive /home/hostedfolders/usmantel/tmp/uploads/1 s3://neon.hosted.files/usmantel/
+/usr/bin/s3cmd put --recursive /home/hostedfolders/familytalk/tmp/uploads/1 s3://neon.hosted.files/familytalk/
 /usr/bin/s3cmd put --recursive /home/hostedfolders/expectel/tmp/1 s3://neon.hosted.files/expectel/
-/usr/bin/s3cmd put --recursive /home/hostedfolders/sipsynergy/tmp/1 s3://neon.hosted.files/sipsynergy/
-/usr/bin/s3cmd put --recursive /home/hostedfolders/telpeer/tmp/1 s3://neon.hosted.files/telpeer/
-/usr/bin/s3cmd put --recursive /home/hostedfolders/associatesmarketing/tmp/1 s3://neon.hosted.files/associatesmarketing/
-/usr/bin/s3cmd put --recursive /home/hostedfolders/imedia/tmp/1 s3://neon.hosted.files/imedia/
-/usr/bin/s3cmd put --recursive /home/hostedfolders/jelltel/tmp/1 s3://neon.hosted.files/jelltel/
+/usr/bin/s3cmd put --recursive /home/hostedfolders/sipsynergy/tmp/uploads/1 s3://neon.hosted.files/sipsynergy/
+/usr/bin/s3cmd put --recursive /home/hostedfolders/telpeer/tmp/uploads/1 s3://neon.hosted.files/telpeer/
+/usr/bin/s3cmd put --recursive /home/hostedfolders/associatesmarketing/tmp s3://neon.hosted.files/associatesmarketing/
+/usr/bin/s3cmd put --recursive /home/hostedfolders/imedia/tmp/uploads/1 s3://neon.hosted.files/imedia/
 /usr/bin/s3cmd put --recursive /home/hostedfolders/1worldtec/tmp/1 s3://neon.hosted.files/1worldtec/
+/usr/bin/s3cmd put --recursive /home/hostedfolders/teluxhd/tmp/uploads/1 s3://neon.hosted.files/teluxhd/
+/usr/bin/s3cmd put --recursive /home/hostedfolders/hotwiretellecom/tmp/uploads/1 s3://neon.hosted.files/hotwiretellecom/
+
+
+/usr/bin/s3cmd put --recursive /var/www/html s3://neon.hosted.files/html/
