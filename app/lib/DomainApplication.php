4c13b1444a63fe805f9882d7a8122129f4233d3c
vishal <vishal.jagani@code-desk.com>
2018-01-31 19:34:37 +0530

    revert language path set changes

--- app/lib/DomainApplication.php
+++
@@ -1,7 +0,0 @@
-<?php namespace App;
-
-class DomainApplication extends \Illuminate\Foundation\Application {
-    public function langPath() {
-        return __DIR__.'/../../../vishal.neon/app/lang';
-    }
-}
