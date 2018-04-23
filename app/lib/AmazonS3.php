1a579ecc2e0cde555327a513d24e04d48316b504
vishal <vishal.jagani@code-desk.com>
2018-04-23 15:55:32 +0530

    Digital signing PDF - NEON-1396

--- app/lib/AmazonS3.php
+++ app/lib/AmazonS3.php
@@ -102,17 +102,17 @@ class AmazonS3 {
      * Generate Path
      * Ex. WaveTell/18-Y/VendorUploads/2015/05
      * */
-    static function generate_upload_path($dir ='',$accountId = '',$CompanyID ) {
+    static function generate_upload_path($dir ='',$accountId = '',$CompanyID, $noDateFolders=false) {
 
         if(empty($dir))
             return false;
 
-        $path = self::generate_path($dir,$CompanyID,$accountId);
+        $path = self::generate_path($dir,$CompanyID,$accountId, $noDateFolders);
 
         return $path;
     }
 
-    static function generate_path($dir ='',$companyId , $accountId = '' ) {
+    static function generate_path($dir ='',$companyId , $accountId = '', $noDateFolders=false ) {
         $UPLOADPATH = CompanyConfiguration::get($companyId,'UPLOAD_PATH');
         $path = $companyId  ."/";
 
@@ -120,7 +120,11 @@ class AmazonS3 {
             $path .= $accountId ."/";
         }
 
-        $path .=  $dir . "/". date("Y")."/".date("m") ."/" .date("d") ."/";
+        if($noDateFolders){
+            $path .=  $dir . "/";
+        }else{
+            $path .=  $dir . "/". date("Y")."/".date("m") ."/" .date("d") ."/";
+        }
         $dir = $UPLOADPATH . '/'. $path;
         if (!file_exists($dir)) {
             //exec("chmod -R 777 " . getenv('UPLOAD_PATH'));
