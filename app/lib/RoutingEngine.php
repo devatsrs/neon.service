60195331a81ef180562ca3b79e3722752cccc868
shamw4r <ahtshamb4u@hotmail.com>
2018-12-28 19:24:04 -0800

    repush the code

--- app/lib/RoutingEngine.php
+++ app/lib/RoutingEngine.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\View;
 
 
 class RoutingEngine extends \Eloquent {
+    
     protected $connection = 'neon_routingengine';
     protected $fillable = [];
     protected $guarded = array('LCRHeaderID');
