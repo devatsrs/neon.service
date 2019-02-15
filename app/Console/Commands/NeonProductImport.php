275b578419df0aa43ed16aa5b7608164e4a84818
shamw4r <ahtshamb4u@hotmail.com>
2019-02-15 16:55:09 +0500

    change the variable name

--- app/Console/Commands/NeonProductImport.php
+++ app/Console/Commands/NeonProductImport.php
@@ -209,7 +209,7 @@ class NeonProductImport extends Command {
             
             //Insert other Company Packages
             $result = DB::connection('sqlsrv')->select("CALL  Prc_ImportPackagestemp( '" . $CompanyID . "','" . $PackageId . "')");
-            $result = DB::connection('sqlsrv')->select("CALL  Prc_ImportProducttemp( '" . $CompanyID . "','" . $ProductID . "')");
+            $result = DB::connection('sqlsrv')->select("CALL  Prc_ImportProducttemp( '" . $CompanyID . "','" . $FieldsProductID . "')");
             
             Log::info('z_neonproductimport Next step in  api/Products service.');
             //Track The Log          
