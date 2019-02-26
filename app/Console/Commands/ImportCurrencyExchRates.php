6a17a96b9d6ffdb488dd67d9cfdcf5a1ceab13e7
Umer <umer.codedesk@gmail.com>
2019-02-26 18:08:12 +0500

    no message

--- app/Console/Commands/ImportCurrencyExchRates.php
+++ app/Console/Commands/ImportCurrencyExchRates.php
@@ -111,7 +111,7 @@ class ImportCurrencyExchRates extends Command {
 	public function handle()
 	{
 
-		Log::useFiles(storage_path() . '/logs/CurrencyExchangeRate-Success-1-' . date('Y-m-d') . '.log');
+	//	Log::useFiles(storage_path() . '/logs/CurrencyExchangeRate-Success-1-' . date('Y-m-d') . '.log');
         Log::info('line 1');
 		Log::info('line 2');
 
