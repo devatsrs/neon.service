34181f0c0410a5a0694ad0e611b317c7e1291733
Umer <umer.codedesk@gmail.com>
2019-04-23 14:44:46 +0500

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -139,7 +139,7 @@ class Kernel extends ConsoleKernel {
 		'App\Console\Commands\UpdateActiveCallCost',
 		'App\Console\Commands\ImportCurrencyExchRates',
 		'App\Console\Commands\ApproveOutPayment',
-
+        'App\Console\Commands\TranslationsImport',
 	];
 
 	/**
