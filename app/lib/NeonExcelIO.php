de24c038ef6645acce93e4e1c0016933f2c82cb9
Vasim Seta <vasim.seta@code-desk.com>
2019-04-26 18:49:56 +0530

    [fix] [SI1-270] Rate Upload slow issue fixed* (https://codedesk.atlassian.net/browse/SI1-270)

--- app/lib/NeonExcelIO.php
+++ app/lib/NeonExcelIO.php
@@ -148,14 +148,12 @@ class NeonExcelIO
             return $this->read_csv($this->file,$limit);
         }
         if($this->file_type == self::$EXCEL){
-
-            //return $this->read_excel($this->file,$limit);
-            return $this->readExcel($this->file,$limit);
+            //return $this->readExcel($this->file,$limit);
+            return $this->readExcel2($this->file,$limit);
         }
 		if($this->file_type == self::$EXCELs){
-
-            //return $this->read_xls_excel($this->file,$limit);
-            return $this->readExcel($this->file,$limit);
+            //return $this->readExcel($this->file,$limit);
+            return $this->readExcel2($this->file,$limit);
         }
 
     }
@@ -1171,4 +1169,56 @@ class NeonExcelIO
 
         return $result;
     }
+
+    //same function in service when change this need to change in service too
+    public function readExcel2($filepath,$limit=0) {
+        $start_time1 = round(microtime(true) * 1000);
+
+        $reader = ReaderFactory::create(Type::XLSX); // for XLSX files
+        $reader->setShouldFormatDates(true);
+        $reader->open($filepath);
+
+        $i=0;$all_rows=[];
+        foreach ($reader->getSheetIterator() as $sheet) {
+            if($sheet->getName() == $this->Sheet) {
+                foreach ($sheet->getRowIterator() as $row) {
+                    $i++;
+
+                    if ($i <= self::$start_row) continue;
+                    if ($limit != 0 && $i > (self::$start_row+$limit)) break;
+
+                    $all_rows[] = $row;
+                }
+            }
+        }
+
+        if($this->first_row == self::$COLUMN_NAMES) {
+            $result = $first_row = array();
+
+            $i = 0;
+            foreach ($all_rows as $row) {
+                if ($i == 0) {
+                    $first_row = $row;
+                } else {
+                    $j = 0;
+                    foreach ($row as $column) {
+                        $result[$i - 1][$first_row[$j]] = $column;
+                        $j++;
+                    }
+                }
+                $i++;
+            }
+        } else {
+            $result = $all_rows;
+        }
+
+        $reader->close();
+
+        $end_time1 = round(microtime(true) * 1000);
+        $process_time1 = ($end_time1 - $start_time1) / 1000;
+        Log::info('readExcel2 call time : ' . $process_time1 . ' Seconds');
+
+        return $result;
+    }
+
 }
