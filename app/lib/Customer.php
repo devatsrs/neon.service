8497e9e20f9230d2e5f88c5cfab7fd15db024719
badal solanki <badals.srs@gmail.com>
2019-05-08 19:00:58 +0530

    Sippy Rate Push Changes

--- app/lib/Customer.php
+++ app/lib/Customer.php
@@ -414,6 +414,28 @@ class Customer extends \Eloquent {
         return $response;
     }
 
+    public function groupArray($arr, $group, $preserveGroupKey = false, $preserveSubArrays = false) {
+        $temp = array();
+        foreach($arr as $key => $value) {
+            $groupValue = $value[$group];
+            if(!$preserveGroupKey)
+            {
+                unset($arr[$key][$group]);
+            }
+            if(!array_key_exists($groupValue, $temp)) {
+                $temp[$groupValue] = array();
+            }
+
+            if(!$preserveSubArrays){
+                $data = count($arr[$key]) == 1? array_pop($arr[$key]) : $arr[$key];
+            } else {
+                $data = $arr[$key];
+            }
+            $temp[$groupValue][] = $data;
+        }
+        return $temp;
+    }
+
     public static function generatePushVendorSippyRateFile($CompanyID,$cronsetting){
         $response['error']  = $response['message']  = array();
         $file_count         = 0;
