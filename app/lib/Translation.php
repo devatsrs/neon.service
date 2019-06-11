b70e1918e1b53b7172ca1aba35bfa799de3b9ca9
aamarnazir <aamarnazir@code-desk.com>
2019-06-11 05:28:10 -0700

    SI1-315 - big fixing in SI

--- app/lib/Translation.php
+++ app/lib/Translation.php
@@ -89,21 +89,6 @@ class Translation extends \Eloquent {
         return $data_langs;
     }
     public static function update_label($languageCode,$systemname,$value){
-
-       /* $json_file = json_decode($labels->Translation, true);
-        Log::info("update_label ". print_r($json_file,true));
-        Log::info("update_label before". json_encode($json_file));
-        if(empty($json_file) or $json_file == 0){$json_file = array();}
-        $system_name=($systemname);
-
-            if (array_key_exists($systemname, $json_file)) {
-                unset($json_file[$system_name]);
-            }
-        //$val = utf8_encode($value);*/
-      //  $val = $value;
-      //  $system_name=($systemname);
-      //  $json_file[$systemname]= $val;
-
         try {
             Log::info("from model ".$systemname.' '.($value));
             $query = "call prc_ImportTranslatation('" . $languageCode . "',"
@@ -111,15 +96,7 @@ class Translation extends \Eloquent {
                 . "'" .utf8_encode($value)."')";
             Log::info("update_label query:" . $query);
             DB::statement($query);
-       /* Log::info("update_label ID ". $labels->TranslationID . ' ' . print_r($json_file,true));
-        Log::info("update_label json ID ". $labels->TranslationID . ' ' . json_encode($json_file,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES));
-            try {
-                $update = DB::table('tblTranslation')
-                    ->where(['TranslationID' => $labels->TranslationID])
-                    ->update(['Translation' => json_encode($json_file)]);
-               Log::info(json_encode($json_file));*/
                 return true;
             } catch (\Exception $e){Log::info($e->getMessage());return false;}
-        //if($update) {return true;} else {return false;}
     }
 }
