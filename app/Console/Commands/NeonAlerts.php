7bb83590e0b0abcc754d2610d9ddcaf23904ed5a
bilalblaxi11 <bilalblaxi@gmail.com>
2019-07-05 14:48:15 +0500

    fixed system alert message concatenation issue

--- app/Console/Commands/NeonAlerts.php
+++ app/Console/Commands/NeonAlerts.php
@@ -70,13 +70,20 @@ class NeonAlerts extends Command
             $cronjobdata = [];
             foreach($Companies as $company) {
                 $CompanyID = $company->CompanyID;
-                $cronjobdata = NeonAlert::neon_alerts($CompanyID, $ProcessID);
+                $cronjobdata[] = NeonAlert::neon_alerts($CompanyID, $ProcessID);
             }
-            if (count($cronjobdata)) {
-                $joblogdata['Message'] = 'Message : ' . implode(',<br>', $cronjobdata);
+
+            $messages = [];
+            foreach ($cronjobdata as $item) {
+                if (count($item))
+                    $messages[] = $item;
+            }
+
+            if (count($messages)) {
+                $joblogdata['Message'] = 'Message : ' . implode(',<br>', $messages);
                 $joblogdata['CronJobStatus'] = CronJob::CRON_FAIL;
                 if (!empty($cronsetting['ErrorEmail'])) {
-                    $result = CronJob::CronJobErrorEmailSend($CronJobID, implode(',\n\r', $cronjobdata));
+                    $result = CronJob::CronJobErrorEmailSend($CronJobID, implode(',\n\r', $messages));
                     Log::error("**Email Sent Status " . $result['status']);
                     Log::error("**Email Sent message " . $result['message']);
                 }
