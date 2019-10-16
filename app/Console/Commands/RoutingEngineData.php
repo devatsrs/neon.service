051e42c7883a8ecd7b60c0c0d889269f48b31a31
Dev <shriramsoft@gmail.com>
2019-10-16 17:27:03 +0530

    routing_engine_data cronjob updated

--- app/Console/Commands/RoutingEngineData.php
+++ app/Console/Commands/RoutingEngineData.php
@@ -64,47 +64,53 @@ class RoutingEngineData extends Command {
         
         $CronJob =  CronJob::find($CronJobID);
         $cronsetting = json_decode($CronJob->Settings,true);
+
         CronJob::activateCronJob($CronJob);
         CronJob::createLog($CronJobID);
         
-        Log::useFiles(storage_path() . '/logs/RoutingEngineData-companyid:'.$CompanyID . '-cronjobid:'.$CronJobID.'-' . date('Y-m-d') . '.log');
+        Log::useFiles(storage_path() . '/logs/RoutingEngineData-'.$CompanyID . '-'. date('Y-m-d') . '.log');
+
         $joblogdata = array();
         $joblogdata['CronJobID'] = $CronJobID;
         $joblogdata['created_at'] = date('Y-m-d H:i:s');
         $joblogdata['created_by'] = 'RMScheduler';
+
         try{
-            
-            //Start Transaction
-            DB::connection('neon_routingengine')->beginTransaction();
+
             CronJob::createLog($CronJobID);
 
-            //Run The Procedure
-            $runPro = DB::connection('sqlsrv')->select('call Prc_RoutingDataPerRow()');
+            //Start Transaction
+            DB::beginTransaction();
+            DB::connection('neon_routingengine')->beginTransaction();
+
+            DB::connection('sqlsrv')->select('call Prc_RoutingDataPerRow()');
             
-            //Track The Log          
+
+            DB::connection('neon_routingengine')->commit();
+            DB::commit();
+
             $joblogdata['Message'] = 'RoutingEngineData Successfully Done';
             $joblogdata['CronJobStatus'] = CronJob::CRON_SUCCESS;
             CronJobLog::insert($joblogdata);
-            DB::connection('neon_routingengine')->commit(); 
-            
-            $result = CronJob::CronJobSuccessEmailSend($CronJobID);
+
+            CronJob::CronJobSuccessEmailSend($CronJobID);
             
         }catch (\Exception $e){
-            Log::useFiles(storage_path() . '/logs/RoutingEngineData-Error-' . date('Y-m-d') . '.log');
-            
-            Log::error($e);
-            $this->info('Failed:' . $e->getMessage());
-            $joblogdata['Message'] ='Error:'.$e->getMessage();
-            $joblogdata['CronJobStatus'] = CronJob::CRON_FAIL;
-            CronJobLog::insert($joblogdata);
-            if(!empty($cronsetting['ErrorEmail'])) {
-                $result = CronJob::CronJobErrorEmailSend($CronJobID,$e);
-                Log::error("**Email Sent Status " . $result['status']);
-                Log::error("**Email Sent message " . $result['message']);
-            }
+
+                Log::error($e);
+                $this->info('Failed:' . $e->getMessage());
+                $joblogdata['Message'] ='Error:'.$e->getMessage();
+                $joblogdata['CronJobStatus'] = CronJob::CRON_FAIL;
+                CronJobLog::insert($joblogdata);
+
+                if(!empty($cronsetting['ErrorEmail'])) {
+                    $result = CronJob::CronJobErrorEmailSend($CronJobID,$e);
+                    Log::error("**Email Sent Status " . $result['status']);
+                    Log::error("**Email Sent message " . $result['message']);
+                }
 
 
-    }
+        }
     
         CronJob::deactivateCronJob($CronJob);
         CronHelper::after_cronrun($this->name, $this);
