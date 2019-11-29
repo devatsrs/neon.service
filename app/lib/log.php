eb0b64dad31078177a547204cf9dfd052f65797b
Haroon <haroon.waheed.codedesk@gmail.com>
2019-11-29 16:22:10 +0500

    stop log system

---
+++ app/lib/log.php
@@ -0,0 +1,83 @@
+<?php
+namespace App\Lib;
+class log extends \Illuminate\Log\Writer
+{
+
+    public function alert($message, array $context = array())
+	{
+		return false;
+	}
+
+	/**
+	 * Log a critical message to the logs.
+	 *
+	 * @param  string  $message
+	 * @param  array  $context
+	 * @return void
+	 */
+	public function critical($message, array $context = array())
+	{
+		return false;
+	}
+
+	/**
+	 * Log an error message to the logs.
+	 *
+	 * @param  string  $message
+	 * @param  array  $context
+	 * @return void
+	 */
+	public function error($message, array $context = array())
+	{
+		return false;
+	}
+
+	/**
+	 * Log a warning message to the logs.
+	 *
+	 * @param  string  $message
+	 * @param  array  $context
+	 * @return void
+	 */
+	public function warning($message, array $context = array())
+	{
+		return false;
+	}
+
+	/**
+	 * Log a notice to the logs.
+	 *
+	 * @param  string  $message
+	 * @param  array  $context
+	 * @return void
+	 */
+	public function notice($message, array $context = array())
+	{
+		return false;
+	}
+
+	/**
+	 * Log an informational message to the logs.
+	 *
+	 * @param  string  $message
+	 * @param  array  $context
+	 * @return void
+	 */
+	public function info($message, array $context = array())
+	{
+		return false;
+	}
+
+	/**
+	 * Log a debug message to the logs.
+	 *
+	 * @param  string  $message
+	 * @param  array  $context
+	 * @return void
+	 */
+	public function debug($message, array $context = array())
+	{
+		return false;
+	}
+
+}
