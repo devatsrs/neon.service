b1edbc4d40a2568bbafe517276466a1f2b26e735
Dev <shriramsoft@gmail.com>
2019-12-10 11:51:59 +0530

    neon web pull

--- public/neon_service_pull.sh
+++ public/neon_service_pull.sh
@@ -7,8 +7,9 @@ sudo chown -R neon_sys:apache /vol/data/speakintelligent.neon.service
 sudo find /vol/data/speakintelligent.neon.service -type d -exec chmod 575 {} +
 sudo find /vol/data/speakintelligent.neon.service -type f -exec chmod 464 {} +
 
-touch /vol/data/cron_job_speakintelligent
+#touch /vol/data/cron_job_speakintelligent
 
 sudo chmod -R 770 /vol/data/speakintelligent.neon.service/storage
-sudo chown -R neon_sys:apache /vol/data/cron_job_speakintelligent
+sudo chown neon_sys:apache /vol/data/cron_job_speakintelligent
+
 echo "success"
